b7bad46be9f50aa0537eb2764a61df38cd776f25
Андрей <mazurenkoam.93@yandex.ru>
2025-04-02 22:20:43 +0300

    Создать каркас модели данных

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,19 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-/**
- * Film.
- */
+import java.time.Duration;
+import java.time.Instant;
+
 @Getter
 @Setter
+@Data
 public class Film {
+    Long id;
+    String name;
+    String description;
+    Instant releaseDate;
+    Duration duration;
 }
