5af9886b9156b5add7139a0a2d8c61f484cf44f3
Андрей <mazurenkoam.93@yandex.ru>
2025-04-04 16:08:34 +0300

    Сделать логирование для операций в REST-контроллерах

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
@@ -11,41 +12,78 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestController
+@RequestMapping("/films")
 public class FilmController {
     private final Map<Long, Film> films = new HashMap<>();
 
     @PostMapping
-    public Film createFilm(@RequestBody Film film) {
-        if (film.getName() == null || film.getName().isBlank()) {
-            throw new ValidationException("Название фильма не может быть пустым");
-        }
-
-        if (film.getDescription() == null || film.getDescription().length() >= 200) {
-            throw new ValidationException("Максимальная длина описания — 200 символов");
-        }
-
-        if (film.getReleaseDate() == null || !checkReleaseDate(film.getReleaseDate())) {
-            throw new ValidationException("Дата релиза — не раньше 28 декабря 1895 года");
-        }
-
-        if (checkDuration(film.getDuration())) {
-            throw new ValidationException("Продолжительность фильма должна быть положительным числом");
-        }
-
+    public Film create(@RequestBody Film film) {
+        validateFilm(film);
         film.setId(getNextId());
         films.put(film.getId(), film);
-
+        log.debug("Фильм {} добавлен!", film);
         return film;
     }
 
+    @PutMapping
+    public Film updateFilm(@RequestBody Film newFilm) {
+        log.debug("Проверка на заполнение поля id {} по условию", newFilm.getId());
+        if (newFilm.getId() == null) {
+            log.error("Id должен быть указан");
+            throw new ValidationException("Id должен быть указан");
+        }
+        if (films.containsKey(newFilm.getId())) {
+            log.debug("Фильмы из хранилища: {}", newFilm);
+            Film oldFilm = films.get(newFilm.getId());
+            oldFilm.setName(newFilm.getName());
+            oldFilm.setDescription(newFilm.getDescription());
+            oldFilm.setReleaseDate(newFilm.getReleaseDate());
+            oldFilm.setDuration(newFilm.getDuration());
+
+            log.debug("Фильм {} обновлен", oldFilm);
+            return oldFilm;
+        }
+        log.error("Фильм с данным id - {} не найден", newFilm.getId());
+        throw new NotFoundException("Фильм с данным id - " + newFilm.getId() + " не найден");
+    }
+
+    @GetMapping
+    public Collection<Film> findAll() {
+        return films.values();
+    }
+
+    private void validateFilm(Film film) {
+        log.debug("Проверка на заполнение поля наименование {} по условию", film.getName());
+        if (film.getName() == null || film.getName().isBlank()) {
+            log.error("Название фильма не может быть пустым");
+            throw new ValidationException("Название фильма не может быть пустым");
+        }
+        log.debug("Проверка на заполнение поля описание {} по условию", film.getDescription());
+        if (film.getDescription() == null || film.getDescription().length() >= 200) {
+            log.error("Максимальная длина описания — 200 символов");
+            throw new ValidationException("Максимальная длина описания — 200 символов");
+        }
+        log.debug("Проверка на заполнение поля дата релиза {} по условию", film.getReleaseDate());
+        if (film.getReleaseDate() == null || !checkReleaseDate(film.getReleaseDate())) {
+            log.error("Дата релиза — не раньше 28 декабря 1895 года");
+            throw new ValidationException("Дата релиза — не раньше 28 декабря 1895 года");
+        }
+        log.debug("Проверка на заполнение поля продолжительность фильма {} по условию", film.getDuration());
+        if (checkDuration(film.getDuration())) {
+            log.error("Продолжительность фильма должна быть положительным числом");
+            throw new ValidationException("Продолжительность фильма должна быть положительным числом");
+        }
+    }
+
     private boolean checkReleaseDate(LocalDate releaseDate) {
-        LocalDate dateOfBirthOfCinema = LocalDate.of(1985, 12, 28);
+        LocalDate dateOfBirthOfCinema = LocalDate.of(1895, 12, 28);
         return releaseDate.isAfter(dateOfBirthOfCinema) || releaseDate.isEqual(dateOfBirthOfCinema);
     }
 
-    private boolean checkDuration(Duration duration) {
-        return duration == null || duration.isNegative() || duration.isZero();
+    private boolean checkDuration(Long duration) {
+        return duration == null || duration < 0;
     }
 
     private long getNextId() {
@@ -55,34 +93,5 @@ public class FilmController {
                 .max()
                 .orElse(0);
         return ++currentMaxId;
-    }
-
-    @PutMapping
-    public Film updateFilm(@RequestBody Film newFilm) {
-        if (newFilm.getId() == null) {
-            throw new ValidationException("Id должен быть указан");
-        }
-        if (films.containsKey(newFilm.getId())) {
-            Film oldFilm = films.get(newFilm.getId());
-            if (newFilm.getName() != null) {
-                oldFilm.setName(newFilm.getName());
-            }
-            if (newFilm.getDescription() != null) {
-                oldFilm.setDescription(newFilm.getDescription());
-            }
-            if (newFilm.getReleaseDate() != null) {
-                oldFilm.setReleaseDate(newFilm.getReleaseDate());
-            }
-            if (newFilm.getDuration() != null) {
-                oldFilm.setDuration(newFilm.getDuration());
-            }
-            return oldFilm;
-        }
-        throw new NotFoundException("Фильм с id = " + newFilm.getId() + " не найден");
-    }
-
-    @GetMapping
-    public Collection<Film> findAll() {
-        return films.values();
     }
 }
